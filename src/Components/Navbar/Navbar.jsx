38541e63e5d61c58752fe4bebc00418d38ca8d5c
Zack OConnor <zackary25@gmail.com>
2025-04-29 16:49:10 -0400

    Navbar and ReadingDetails page completed.

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 h-screen w-56 bg-stone-600 text-white flex flex-col items-start px-4 py-6 space-y-4">
+
+        <NavLink
+            to="/" className={({ isActive }) => `w-full px-3 py-2 rounded text-left transition ${ isActive ? "bg-stone-800 font-bold" : "hover:bg-stone-700"}`} > Home
+        </NavLink>
+
+        <NavLink
+            to="/reading" className={({ isActive }) => `w-full px-3 py-2 rounded text-left transition ${ isActive ? "bg-stone-800 font-bold" : "hover:bg-stone-700"}`} > {" "}Reading
+        </NavLink>
+        <NavLink to="/reading-details" className={({ isActive }) => `w-full px-3 py-2 rounded text-left transition ${ isActive ? "bg-stone-800 font-bold" : "hover:bg-stone-700" }`} > {" "} Reading Details
+        </NavLink>
+    </nav>
+  )
+}
+
+export default Navbar
